4a421da6a256f5f19d8c6c320af32606d7a0384b
bed7ime <living1leaf@gmail.com>
2024-09-02 16:47:57 +0700

    complete controller and router

--- backEnd/controllers/financial.controller.js
+++ backEnd/controllers/financial.controller.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const Financial = require("../models/financial.model");
 
 exports.create = async (req, res) => {
@@ -19,6 +20,85 @@ exports.create = async (req, res) => {
       res.status(500).send({
         message:
           error.message || "An error occurred while saving a new record.",
+      });
+    });
+};
+
+exports.getAll = async (req, res) => {
+  await Financial.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message || "An error occurred while loading financial records.",
+      });
+    });
+};
+
+exports.findAllbyUserId = async (req, res) => {
+  const userId = req.params.userId;
+
+  await Financial.findAll({ where: { userId: userId } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message || "An error occurred while loading financial records.",
+      });
+    });
+};
+
+exports.getbyId = async (req, res) => {
+  const id = req.params.id;
+
+  await Financial.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message || "An error occurred while loading financial records.",
+      });
+    });
+};
+
+exports.update = async (req, res) => {
+  const id = req.params.id;
+
+  await Financial.update(req.body, { where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Financial update successful." });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message ||
+          "An error occurred while updating financial Id: " + id + ".",
+      });
+    });
+};
+
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+
+  await Financial.destroy({ where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Financial delete successful." });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message ||
+          "An error occurred while deleting financial Id: " + id + ".",
       });
     });
 };
--- backEnd/routers/financial.router.js
+++ backEnd/routers/financial.router.js
@@ -2,6 +2,17 @@ const financialController = require("../controllers/financial.controller");
 const express = require("express");
 const router = express.Router();
 
+// Create record
 router.post("/", financialController.create);
+// Get all records
+router.get("/", financialController.getAll);
+// Get records by userId
+router.get("/users/:userId", financialController.findAllbyUserId);
+// Get record by id
+router.get("/:id", financialController.getbyId);
+// Update record by id
+router.put("/:id", financialController.update);
+// Delete record by id
+router.delete("/:id", financialController.delete);
 
 module.exports = router;
